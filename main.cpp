f96b9c29b5061095d969bde80f3ff0b1cdcb0e6b
Nick Yu <nickyu42@gmail.com>
2022-10-24 15:25:21 +0200

    Add csv capture and analysis notebook

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
+#include "ThisThread.h"
 #include "mbed.h"
+#include <cstdio>
 
 #include "src/config.h"
 #include "src/ina219.h"
 
-#define HEARTBREAT_RATE     500ms
+#define HEARTBREAT_RATE 500ms
+
+Thread read_sensor;
 
 DigitalOut heartbeat_led(LED1);
+
+void read_sensor_task(CurrentSensor *s) 
+{
+    while (true)
+    {
+        printf(
+            "%d,%d,%d,%d\r\n", 
+            s->read_current_raw(), 
+            s->read_bus_voltage_mV(), 
+            s->read_shunt_voltage_raw(),
+            s->read_power_raw()
+        );
+
+        ThisThread::sleep_for(100ms);
+    }
+}
 
 int main()
 {
@@ -15,15 +35,10 @@ int main()
     CurrentSensor current_sensor(INA219_SDA, INA219_SCLK);
     printf("Done!\r\n");
 
+    read_sensor.start(callback(read_sensor_task, &current_sensor));
+
     while (true) {
-        printf(
-            "I=%d Vb=%d (mV) Vs=%d\r\n", 
-            current_sensor.read_current_raw(), 
-            current_sensor.read_bus_voltage_mV(), 
-            current_sensor.read_shunt_voltage_raw()
-        );
-        
-        ThisThread::sleep_for(1s);
+        ThisThread::sleep_for(HEARTBREAT_RATE);
         heartbeat_led = !heartbeat_led;
     }
 }
--- src/transmit.cpp
+++ src/transmit.cpp
@@ -1,73 +1,73 @@
-#include "transmit.h"
+// #include "transmit.h"
 
-#include "mbed.h"
-#include "radio.h"
+// #include "mbed.h"
+// #include "radio.h"
 
-#include "config.h"
+// #include "config.h"
 
-EventQueue queue(4 * EVENTS_EVENT_SIZE);
+// EventQueue queue(4 * EVENTS_EVENT_SIZE);
 
-Thread radioThread;
+// Thread radioThread;
 
-void transmit(uint8_t *data) {
-    Radio::radio.tx_buf[0] = *data;
-    Radio::Send(1, 0, 0, 0);
-}
+// void transmit(uint8_t *data) {
+//     Radio::radio.tx_buf[0] = *data;
+//     Radio::Send(1, 0, 0, 0);
+// }
 
-void queue_transmit(uint8_t data)
-{
-    queue.call(callback(transmit, &data));
-}
+// void queue_transmit(uint8_t data)
+// {
+//     queue.call(callback(transmit, &data));
+// }
 
-void txDoneCB()
-{
-}
+// void txDoneCB()
+// {
+// }
 
-void rxDoneCB(uint8_t size, float rssi, float snr)
-{
-    puts("got response: ");
+// void rxDoneCB(uint8_t size, float rssi, float snr)
+// {
+//     puts("got response: ");
 
-    for (size_t i = 0; i < size; i++) {
-        printf("%c", Radio::radio.rx_buf[i]);
-    }
+//     for (size_t i = 0; i < size; i++) {
+//         printf("%c", Radio::radio.rx_buf[i]);
+//     }
 
-    puts("\r\n");
-}
-
-
-void radio_irq_callback()
-{
-    queue.call(Radio::service);
-}
+//     puts("\r\n");
+// }
 
 
-const RadioEvents_t rev = {
-    /* DioPin_top_half */   radio_irq_callback,
-    /* TxDone_topHalf */    NULL,
-    /* TxDone_botHalf */    txDoneCB,
-    /* TxTimeout  */        NULL,
-    /* RxDone  */           rxDoneCB,
-    /* RxTimeout  */        NULL,
-    /* RxError  */          NULL,
-    /* FhssChangeChannel  */NULL,
-    /* CadDone  */          NULL
-};
+// void radio_irq_callback()
+// {
+//     queue.call(Radio::service);
+// }
 
-void start_LoRa() {
-    printf("init LoRa\r\n");
 
-    Radio::Init(&rev);
+// const RadioEvents_t rev = {
+//     /* DioPin_top_half */   radio_irq_callback,
+//     /* TxDone_topHalf */    NULL,
+//     /* TxDone_botHalf */    txDoneCB,
+//     /* TxTimeout  */        NULL,
+//     /* RxDone  */           rxDoneCB,
+//     /* RxTimeout  */        NULL,
+//     /* RxError  */          NULL,
+//     /* FhssChangeChannel  */NULL,
+//     /* CadDone  */          NULL
+// };
 
-    Radio::Standby();
-    Radio::LoRaModemConfig(BW_KHZ, SPREADING_FACTOR, 1);
-    Radio::SetChannel(CF_HZ);
+// void start_LoRa() {
+//     printf("init LoRa\r\n");
 
-    Radio::set_tx_dbm(TX_DBM);
+//     Radio::Init(&rev);
 
-               // preambleLen, fixLen, crcOn, invIQ
-    Radio::LoRaPacketConfig(8, false, true, false);
+//     Radio::Standby();
+//     Radio::LoRaModemConfig(BW_KHZ, SPREADING_FACTOR, 1);
+//     Radio::SetChannel(CF_HZ);
 
-    radioThread.start([]() {
-        queue.dispatch_forever();
-    });
-}
+//     Radio::set_tx_dbm(TX_DBM);
+
+//                // preambleLen, fixLen, crcOn, invIQ
+//     Radio::LoRaPacketConfig(8, false, true, false);
+
+//     radioThread.start([]() {
+//         queue.dispatch_forever();
+//     });
+// }
