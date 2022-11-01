bb555873fc6bf5e4875d36386b348695c3da34c5
Nick Yu <nickyu42@gmail.com>
2022-11-01 13:37:15 +0100

    change flowrate metric to power

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
+#include <chrono>
 #include <cmath>
+#include <cstddef>
 #include <cstdint>
 #include <cstdio>
 
+#include "BUILD/NUCLEO_F446RE/ARMC6/mbed_config.h"
+#include "BufferedSerial.h"
+#include "PinNameAliases.h"
 #include "ThisThread.h"
 #include "Thread.h"
 #include "mbed.h"
@@ -10,12 +15,21 @@
 #include "src/CurrentSensor.h"
 #include "src/config.h"
 
+using namespace std::chrono;
+
 #define HEARTBREAT_RATE 500ms
 
-#define CURRENT_SENSOR_BUFFER_SIZE 100
-#define CURRENT_SENSOR_READ_DELAY 10ms
+#define CURRENT_SENSOR_BUFFER_SIZE 20
+#define CURRENT_SENSOR_READ_DELAY 500000us
+
+// 4 * int16_t    = 8 bytes
+// 4 comma chars  = 4 bytes
+// 1 double (fr)  = 8 bytes
+// \r\n           = 2 bytes
+#define CURRENT_SENSOR_PACKET_SIZE (4 * 2 + 4 + 8 + 2)
 
 #define CRITICAL_VOLTAGE_mV 7000
+
 
 DigitalOut heartbeat_led(LED1);
 
@@ -23,16 +37,41 @@ Thread read_sensor_thread;
 Ticker read_sensor;
 CharCircularBuffer<CURRENT_SENSOR_BUFFER_SIZE> busvoltage_samples_mV;
 CharCircularBuffer<CURRENT_SENSOR_BUFFER_SIZE> current_samples_raw;
+CharCircularBuffer<CURRENT_SENSOR_BUFFER_SIZE> power_samples;
 float current_flow_rate = 0;
 
 size_t critical_voltage_samples = 0;
+
+BufferedSerial console(USBTX, USBRX, MBED_CONF_PLATFORM_STDIO_BAUD_RATE);
+
+
+uint8_t data_buffer[CURRENT_SENSOR_PACKET_SIZE] = {0};
+
+Timer read_timer;
+
+FileHandle* mbed::mbed_override_console(int)
+{
+    return &console;
+}
+
+double estimate_flowrate(int16_t power_mean) {
+  double a = 148.450205494820409057865617796779;
+  double b = -826.204954144229304802138358354568;
+  return (power_mean - b) / a;
+}
 
 void read_current_task(CurrentSensor *s) {
 
   printf("[INFO] Starting read_current task\r\n");
 
   while (true) {
-    uint16_t busvoltage = s->read_bus_voltage_mV();
+    read_timer.reset();
+    read_timer.start();
+
+    int16_t busvoltage = s->read_bus_voltage_mV();
+    int16_t current = s->read_current_raw();
+    int16_t shuntvoltage = s->read_shunt_voltage_raw();
+    int16_t power = s->read_power_raw();
 
     if (busvoltage > CRITICAL_VOLTAGE_mV) {
       critical_voltage_samples++;
@@ -44,11 +83,38 @@ void read_current_task(CurrentSensor *s) {
       // power_down sequence
     }
 
-    busvoltage_samples_mV.push(static_cast<int16_t>(busvoltage));
-    current_samples_raw.push(s->read_current_raw());
+    // busvoltage_samples_mV.push(static_cast<int16_t>(busvoltage));
+    // current_samples_raw.push(s->read_current_raw());
+    power_samples.push(power);
 
-    current_flow_rate = ceil(busvoltage_samples_mV.mean()) / 1000;
-    ThisThread::sleep_for(CURRENT_SENSOR_READ_DELAY);
+    // current_flow_rate = ceil(busvoltage_samples_mV.mean()) / 1000;
+    current_flow_rate = estimate_flowrate(power_samples.mean());
+
+    // data_buffer[0] = static_cast<uint8_t>(busvoltage & 0xff);
+    // data_buffer[1] = static_cast<uint8_t>((busvoltage >> 8) & 0xff);
+
+    // data_buffer[3] = static_cast<uint8_t>(current & 0xff);
+    // data_buffer[4] = static_cast<uint8_t>((current >> 8) & 0xff);
+
+    // data_buffer[6] = static_cast<uint8_t>(shuntvoltage & 0xff);
+    // data_buffer[7] = static_cast<uint8_t>((shuntvoltage >> 8) & 0xff);
+
+    // data_buffer[9] = static_cast<uint8_t>(power & 0xff);
+    // data_buffer[10] = static_cast<uint8_t>((power >> 8) & 0xff);
+
+    // uint64_t current_flow_rate_uint = static_cast<uint64_t>(current_flow_rate);
+    // for (size_t i = 0; i < 8; i++) {
+    //   data_buffer[12 + i] = static_cast<uint8_t>((current_flow_rate_uint >> (8 * i)) & 0xff);
+    // }
+
+    printf("%d,%d,%d,%d,%f\r\n", busvoltage, current, shuntvoltage, power, current_flow_rate);
+
+    read_timer.stop();
+
+    printf("time taken: %llu us\r\n", duration_cast<microseconds>(read_timer.elapsed_time()).count());
+
+    ThisThread::sleep_for(duration_cast<milliseconds>(
+        CURRENT_SENSOR_READ_DELAY - read_timer.elapsed_time() ));
   }
 }
 
@@ -66,10 +132,18 @@ int main() {
   read_sensor_thread.start(callback(read_current_task, &current_sensor));
   printf("[ OK ] Done!\r\n");
 
+  data_buffer[2] = ',';
+  data_buffer[5] = ',';
+  data_buffer[8] = ',';
+  data_buffer[11] = ',';
+  data_buffer[CURRENT_SENSOR_PACKET_SIZE - 2] = '\r';
+  data_buffer[CURRENT_SENSOR_PACKET_SIZE - 1] = '\n';
+
   while (true) {
     ThisThread::sleep_for(HEARTBREAT_RATE);
     heartbeat_led = !heartbeat_led;
 
-    // printf("[INFO] Measured flowrate: %d l/10min\r\n", static_cast<uint16_t>(current_flow_rate * 10));
+    // printf("[INFO] Measured flowrate: %d l/10min\r\n",
+    // static_cast<uint16_t>(current_flow_rate * 10));
   }
 }
--- src/CharCircularBuffer.h
+++ src/CharCircularBuffer.h
@@ -9,21 +9,23 @@
 template <uint32_t BufferSize> class CharCircularBuffer {
 
 public:
-  CharCircularBuffer() : _total(0), _mean(0), _tail(0), _head(0) {}
+  CharCircularBuffer() : _total(0), _mean(0), _tail(1), _head(0) {}
 
   ~CharCircularBuffer() {}
 
-  void push(const uint16_t &data) {
+  void push(const int16_t &data) {
     core_util_critical_section_enter();
+
+    this->_tail = incrementCounter(this->_tail);
+    this->_head = incrementCounter(this->_head);
 
     this->_buffer[this->_head] = data;
 
-    this->_tail = this->_head;
-    this->_head = incrementCounter(this->_head);
+    this->_total -= static_cast<int64_t>(this->_buffer[this->_tail]);
+    this->_total += static_cast<int64_t>(this->_buffer[this->_head]);
 
-    this->_total -= this->_buffer[this->_tail];
-    this->_total += this->_buffer[this->_head];
-    this->_mean = static_cast<float>(this->_total) / BufferSize;
+    this->_mean =
+        static_cast<double>(this->_total) / static_cast<double>(BufferSize);
 
     core_util_critical_section_exit();
   }
@@ -38,11 +40,11 @@ public:
     return val;
   }
 
-  float mean() { return this->_mean; }
+  double mean() { return this->_mean; }
 
 private:
   int64_t _total;
-  float _mean;
+  double _mean;
 
   int16_t _buffer[BufferSize];
   uint32_t _tail;
